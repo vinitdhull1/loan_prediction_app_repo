5a1d0c0f9e2a88a6709e219201e75c775e95ec62
Vinit Dhull <a7793@ind.aptaracorp.com>
2021-12-08 18:28:54 +0530

    First commit of loan_prediction app

---
+++ loan_prediction_proj/loan_prediction/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    if request.method == "POST":
+        loam_amount = request.POST.get("loan_amount")
+        marital_status = request.POST.get("marital_status")
+        dependents = request.POST.get("dependents")
+        education = request.POST.get("education")
+        employement_status = request.POST.get("employement_status")
+        credit_history = request.POST.get("credit_history")
+        applicant_income = request.POST.get("applicant_income")
+        loan_amount_term = request.POST.get("loan_amount_term")
+        property_area = request.POST.get("property_area")
+        property_area = request.POST.get("property_area")
+    return render(request, 'index.html')
