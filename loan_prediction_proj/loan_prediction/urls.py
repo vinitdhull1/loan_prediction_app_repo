fdb0cfccff9708e8d6aab6829eaaefc394a6a02a
AIMLTeam <kolikaran1992@gmail.com>
2021-12-09 17:18:21 +0530

    changes done

---
+++ loan_prediction_proj/loan_prediction/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+from django.contrib.auth.decorators import login_required
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
